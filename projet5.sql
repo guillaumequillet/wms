58c20d3ce08b4649a9d34e0226ef48019bf0616c
guillaumequillet <bgachats@yahoo.fr>
2019-11-12 16:45:27 +0100

    implemented all classes in DB and exported the sql dump

--- projet5.sql
+++ projet5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  sam. 09 nov. 2019 à 21:20
+-- Généré le :  mar. 12 nov. 2019 à 16:44
 -- Version du serveur :  10.4.6-MariaDB
 -- Version de PHP :  7.3.9
 
@@ -39,20 +39,107 @@ CREATE TABLE `articles` (
   `barcode` varchar(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Déchargement des données de la table `articles`
+-- Structure de la table `incoming`
 --
 
-INSERT INTO `articles` (`id`, `code`, `description`, `weight`, `width`, `height`, `length`, `barcode`) VALUES
-(67, 'BODYSTIM_ELECTR', 'Jeu de 4 électrodes pour Body Stim', 50, 0, 0, 0, '5410984753017'),
-(68, 'ELECT_STIMPLUS', 'Jeu de 2 électrodes pour Stim Plus', 50, 0, 0, 0, '5410984008506'),
-(69, 'AIC_25135', 'Tourne clé facile', 50, 0, 0, 0, '3661474251359'),
-(70, 'AIC_25382', 'Oxymètre de pouls', 50, 0, 0, 0, '4015672106123'),
-(71, 'PCV8005', 'Thermo-hygromètre PCV8005', 55, 0, 0, 0, '4250225760227'),
-(72, 'BES_23400', 'celui là aussi pue grave', 50, 51, 53, 52, '0000000000054'),
-(73, 'FILTRE_HAW501E', 'de la merde', 5, 6, 8, 7, '0000000000009'),
-(74, 'nouveau', 'test 2', 101, 102, 104, 103, '105'),
-(75, 'FAMILY_THERMOME', 'Thermomètre digital familial Family Thermometer', 45, 1, 0, 0, '5410984548002');
+CREATE TABLE `incoming` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `user` int(11) NOT NULL,
+  `provider` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `user` int(11) NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lines`
+--
+
+CREATE TABLE `lines` (
+  `id` int(11) NOT NULL,
+  `article` int(11) NOT NULL,
+  `location` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `locations`
+--
+
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL,
+  `area` varchar(5) NOT NULL,
+  `aisle` varchar(5) NOT NULL,
+  `col` varchar(5) NOT NULL,
+  `level` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `outgoing`
+--
+
+CREATE TABLE `outgoing` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `reference` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `user` int(11) NOT NULL,
+  `recipient` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zipcode` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stocks`
+--
+
+CREATE TABLE `stocks` (
+  `id` int(11) NOT NULL,
+  `location` int(11) NOT NULL,
+  `article` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables déchargées
@@ -65,6 +152,55 @@ ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `incoming`
+--
+ALTER TABLE `incoming`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Index pour la table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Index pour la table `lines`
+--
+ALTER TABLE `lines`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `article` (`article`),
+  ADD KEY `location` (`location`);
+
+--
+-- Index pour la table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `outgoing`
+--
+ALTER TABLE `outgoing`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Index pour la table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location` (`location`),
+  ADD KEY `article` (`article`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -72,7 +208,85 @@ ALTER TABLE `articles`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `incoming`
+--
+ALTER TABLE `incoming`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `lines`
+--
+ALTER TABLE `lines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `outgoing`
+--
+ALTER TABLE `outgoing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `stocks`
+--
+ALTER TABLE `stocks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `incoming`
+--
+ALTER TABLE `incoming`
+  ADD CONSTRAINT `incoming_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `lines`
+--
+ALTER TABLE `lines`
+  ADD CONSTRAINT `lines_ibfk_1` FOREIGN KEY (`article`) REFERENCES `articles` (`id`),
+  ADD CONSTRAINT `lines_ibfk_2` FOREIGN KEY (`location`) REFERENCES `locations` (`id`);
+
+--
+-- Contraintes pour la table `outgoing`
+--
+ALTER TABLE `outgoing`
+  ADD CONSTRAINT `outgoing_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD CONSTRAINT `stocks_ibfk_1` FOREIGN KEY (`location`) REFERENCES `locations` (`id`),
+  ADD CONSTRAINT `stocks_ibfk_2` FOREIGN KEY (`article`) REFERENCES `articles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
