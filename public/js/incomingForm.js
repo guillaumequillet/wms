74ffa8e1e10d77790599f4adf58c3ee3391d5bfc
guillaumequillet <bgachats@yahoo.fr>
2019-11-22 08:41:17 +0100

    added dynamic order rows creation in javascript

--- public/js/incomingForm.js
+++ public/js/incomingForm.js
@@ -1,9 +1,33 @@
 // incomingForm
+let rowNumber = 0;
 let orderRow = document.querySelector('#incomingForm div.orderRow');
 let orderRowsParent = orderRow.parentNode;
+orderRowsParent.removeChild(orderRow);
 let addButton = document.querySelector("#addRowButton");
 
-addButton.addEventListener("click", e=> {
+function addRow() {
+    let newChild = orderRow.cloneNode(true);
+    orderRowsParent.appendChild(newChild);
+    let totalRows = orderRowsParent.querySelectorAll("div.orderRow").length;
+
+    Array.from(newChild.querySelectorAll("div.orderRow input")).forEach(function(element, i) {
+        element.name = element.name + rowNumber;
+    });
+
+    rowNumber++;
+
+    let button = newChild.querySelector(".button");
+
+    button.addEventListener("click", e => {
+        e.preventDefault();
+        orderRowsParent.removeChild(newChild);
+    })    
+}
+
+// adding at least one line
+// addRow(); 
+
+addButton.addEventListener("click", e => {
     e.preventDefault();
-    orderRowsParent.appendChild(orderRow.cloneNode(true));
+    addRow();
 });
